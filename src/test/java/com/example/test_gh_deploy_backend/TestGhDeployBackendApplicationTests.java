09c0f12cdc226b2fe3a8c687e923da8847376aa3
xLillium <nicolas.motillon@gmail.com>
2025-04-10 14:29:11 +0200

    test: tests are failing

--- src/test/java/com/example/test_gh_deploy_backend/TestGhDeployBackendApplicationTests.java
+++ src/test/java/com/example/test_gh_deploy_backend/TestGhDeployBackendApplicationTests.java
@@ -2,12 +2,15 @@ package com.example.test_gh_deploy_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class TestGhDeployBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(false).isFalse();
 	}
 
 }
