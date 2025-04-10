22f3dd9b45efbb6428f56f54c9c292876b33557e
xLillium <nicolas.motillon@gmail.com>
2025-04-10 13:53:07 +0200

    init

---
+++ src/main/java/com/example/test_gh_deploy_backend/TestGhDeployBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_gh_deploy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGhDeployBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGhDeployBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_gh_deploy_backend/TestGhDeployBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_gh_deploy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGhDeployBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
