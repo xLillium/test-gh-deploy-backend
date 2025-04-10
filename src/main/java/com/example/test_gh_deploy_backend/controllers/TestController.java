f827846ec1c3e7862d1cf90e5a925a984d70cc4e
xLillium <nicolas.motillon@gmail.com>
2025-04-10 18:20:31 +0200

    feat: add test endpoint (#5)

---
+++ src/main/java/com/example/test_gh_deploy_backend/controllers/TestController.java
@@ -0,0 +1,15 @@
+package com.example.test_gh_deploy_backend.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+    @GetMapping
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Test OK");
+    }
+}
